b08ecf6a43da1449cd1ac85fb40a80375c349eb6
Sinisa Vukovic <sinisa503@gmail.com>
2017-05-23 18:25:15 +0200

    Initial Commit

---
+++ UrlSchemeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UrlSchemeApp
+//
+//  Created by Sinisa Vukovic on 23/05/2017.
+//  Copyright © 2017 Sinisa Vukovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
